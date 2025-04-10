30c1d40e13ab5ccb64e16bda066dd822a9cc1e89
Mateus Brebal <mateus.brebal@gmail.com>
2025-04-10 19:56:18 -0300

    Inicializando Repositório JOGO DA FORCA.

---
+++ UC11_Jogo_Forca/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace UC11_Jogo_Forca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelCONFIG = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonINICIA = new System.Windows.Forms.Button();
+            this.textBoxPALAVRASECRETA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNOMEJOGADOR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labelNOMEJOGADOR = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelPONTOS = new System.Windows.Forms.Label();
+            this.labelCONFIG = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBoxFORCA = new System.Windows.Forms.PictureBox();
+            this.labelpalavraSECRETA = new System.Windows.Forms.Label();
+            this.textBoxLETRA = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonTESTAR = new System.Windows.Forms.Button();
+            this.labelX = new System.Windows.Forms.Label();
+            this.panelCONFIG.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFORCA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelCONFIG
+            // 
+            this.panelCONFIG.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelCONFIG.Controls.Add(this.labelX);
+            this.panelCONFIG.Controls.Add(this.label3);
+            this.panelCONFIG.Controls.Add(this.buttonINICIA);
+            this.panelCONFIG.Controls.Add(this.textBoxPALAVRASECRETA);
+            this.panelCONFIG.Controls.Add(this.label2);
+            this.panelCONFIG.Controls.Add(this.textBoxNOMEJOGADOR);
+            this.panelCONFIG.Controls.Add(this.label1);
+            this.panelCONFIG.Location = new System.Drawing.Point(-3, 177);
+            this.panelCONFIG.Name = "panelCONFIG";
+            this.panelCONFIG.Size = new System.Drawing.Size(505, 135);
+            this.panelCONFIG.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(60, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(380, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Informe nesta tela o Nome do Jogador e a Palavra Secreta a ser jogada!";
+            // 
+            // buttonINICIA
+            // 
+            this.buttonINICIA.BackColor = System.Drawing.Color.SteelBlue;
+            this.buttonINICIA.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.buttonINICIA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonINICIA.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonINICIA.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonINICIA.Location = new System.Drawing.Point(147, 86);
+            this.buttonINICIA.Name = "buttonINICIA";
+            this.buttonINICIA.Size = new System.Drawing.Size(207, 23);
+            this.buttonINICIA.TabIndex = 2;
+            this.buttonINICIA.Text = "Iniciar o Jogo!";
+            this.buttonINICIA.UseVisualStyleBackColor = false;
+            this.buttonINICIA.Click += new System.EventHandler(this.buttonINICIA_Click);
+            // 
+            // textBoxPALAVRASECRETA
+            // 
+            this.textBoxPALAVRASECRETA.Location = new System.Drawing.Point(147, 60);
+            this.textBoxPALAVRASECRETA.Name = "textBoxPALAVRASECRETA";
+            this.textBoxPALAVRASECRETA.Size = new System.Drawing.Size(207, 20);
+            this.textBoxPALAVRASECRETA.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Palavra Secreta:";
+            // 
+            // textBoxNOMEJOGADOR
+            // 
+            this.textBoxNOMEJOGADOR.Location = new System.Drawing.Point(147, 34);
+            this.textBoxNOMEJOGADOR.Name = "textBoxNOMEJOGADOR";
+            this.textBoxNOMEJOGADOR.Size = new System.Drawing.Size(207, 20);
+            this.textBoxNOMEJOGADOR.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome do Jogador:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Brown;
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.labelNOMEJOGADOR);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.labelPONTOS);
+            this.panel2.Controls.Add(this.labelCONFIG);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(496, 33);
+            this.panel2.TabIndex = 7;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Image = global::UC11_Jogo_Forca.Properties.Resources.jogo_forca;
+            this.label9.Location = new System.Drawing.Point(3, 2);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 29);
+            this.label9.TabIndex = 10;
+            // 
+            // labelNOMEJOGADOR
+            // 
+            this.labelNOMEJOGADOR.Font = new System.Drawing.Font("Microsoft JhengHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNOMEJOGADOR.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.labelNOMEJOGADOR.Location = new System.Drawing.Point(161, 2);
+            this.labelNOMEJOGADOR.Name = "labelNOMEJOGADOR";
+            this.labelNOMEJOGADOR.Size = new System.Drawing.Size(132, 29);
+            this.labelNOMEJOGADOR.TabIndex = 9;
+            this.labelNOMEJOGADOR.Text = "Aroldo Cruz";
+            this.labelNOMEJOGADOR.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label7.Location = new System.Drawing.Point(107, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 15);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Jogador";
+            // 
+            // labelPONTOS
+            // 
+            this.labelPONTOS.Font = new System.Drawing.Font("Microsoft JhengHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPONTOS.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.labelPONTOS.Location = new System.Drawing.Point(356, 2);
+            this.labelPONTOS.Name = "labelPONTOS";
+            this.labelPONTOS.Size = new System.Drawing.Size(53, 29);
+            this.labelPONTOS.TabIndex = 7;
+            this.labelPONTOS.Text = "100";
+            this.labelPONTOS.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelCONFIG
+            // 
+            this.labelCONFIG.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCONFIG.Image = global::UC11_Jogo_Forca.Properties.Resources.engrenagem_branca;
+            this.labelCONFIG.Location = new System.Drawing.Point(465, 2);
+            this.labelCONFIG.Name = "labelCONFIG";
+            this.labelCONFIG.Size = new System.Drawing.Size(27, 29);
+            this.labelCONFIG.TabIndex = 6;
+            this.labelCONFIG.Click += new System.EventHandler(this.labelCONFIG_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Location = new System.Drawing.Point(314, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Pontos";
+            // 
+            // pictureBoxFORCA
+            // 
+            this.pictureBoxFORCA.Image = global::UC11_Jogo_Forca.Properties.Resources.vazia;
+            this.pictureBoxFORCA.InitialImage = null;
+            this.pictureBoxFORCA.Location = new System.Drawing.Point(6, 39);
+            this.pictureBoxFORCA.Name = "pictureBoxFORCA";
+            this.pictureBoxFORCA.Size = new System.Drawing.Size(112, 132);
+            this.pictureBoxFORCA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxFORCA.TabIndex = 0;
+            this.pictureBoxFORCA.TabStop = false;
+            // 
+            // labelpalavraSECRETA
+            // 
+            this.labelpalavraSECRETA.Font = new System.Drawing.Font("Microsoft JhengHei", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelpalavraSECRETA.ForeColor = System.Drawing.Color.Maroon;
+            this.labelpalavraSECRETA.Location = new System.Drawing.Point(12, 184);
+            this.labelpalavraSECRETA.Name = "labelpalavraSECRETA";
+            this.labelpalavraSECRETA.Size = new System.Drawing.Size(474, 47);
+            this.labelpalavraSECRETA.TabIndex = 11;
+            this.labelpalavraSECRETA.Text = "M";
+            this.labelpalavraSECRETA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxLETRA
+            // 
+            this.textBoxLETRA.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLETRA.Location = new System.Drawing.Point(226, 246);
+            this.textBoxLETRA.MaxLength = 1;
+            this.textBoxLETRA.Name = "textBoxLETRA";
+            this.textBoxLETRA.Size = new System.Drawing.Size(43, 44);
+            this.textBoxLETRA.TabIndex = 6;
+            this.textBoxLETRA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(112, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Escolha uma Letra!";
+            // 
+            // buttonTESTAR
+            // 
+            this.buttonTESTAR.BackColor = System.Drawing.Color.Maroon;
+            this.buttonTESTAR.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.buttonTESTAR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonTESTAR.Font = new System.Drawing.Font("Microsoft JhengHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTESTAR.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonTESTAR.Location = new System.Drawing.Point(275, 246);
+            this.buttonTESTAR.Name = "buttonTESTAR";
+            this.buttonTESTAR.Size = new System.Drawing.Size(87, 44);
+            this.buttonTESTAR.TabIndex = 6;
+            this.buttonTESTAR.Text = "Testar!";
+            this.buttonTESTAR.UseVisualStyleBackColor = false;
+            this.buttonTESTAR.Click += new System.EventHandler(this.buttonTESTAR_Click);
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX.ForeColor = System.Drawing.Color.Maroon;
+            this.labelX.Location = new System.Drawing.Point(481, 8);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(14, 15);
+            this.labelX.TabIndex = 6;
+            this.labelX.Text = "X";
+            this.labelX.Click += new System.EventHandler(this.labelX_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(498, 309);
+            this.Controls.Add(this.panelCONFIG);
+            this.Controls.Add(this.buttonTESTAR);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxLETRA);
+            this.Controls.Add(this.labelpalavraSECRETA);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBoxFORCA);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Forca";
+            this.panelCONFIG.ResumeLayout(false);
+            this.panelCONFIG.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFORCA)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxFORCA;
+        private System.Windows.Forms.Panel panelCONFIG;
+        private System.Windows.Forms.Button buttonINICIA;
+        private System.Windows.Forms.TextBox textBoxPALAVRASECRETA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxNOMEJOGADOR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelPONTOS;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelCONFIG;
+        private System.Windows.Forms.Label labelNOMEJOGADOR;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labelpalavraSECRETA;
+        private System.Windows.Forms.TextBox textBoxLETRA;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonTESTAR;
+        private System.Windows.Forms.Label labelX;
+    }
+}
+
---
+++ UC11_Jogo_Forca/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UC11_Jogo_Forca
+{
+    public partial class Form1 : Form
+    {
+        string palavraSECRETA = string.Empty;
+        string[] vetorpalavraSECRETA = new string[10];
+        string letraTESTAR = "";
+        int erro = 1;
+        int acerto = 0;
+        int tentativas = 0;
+        int pontos = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            panelCONFIG.Visible = false;
+            labelNOMEJOGADOR.Text = string.Empty;
+            labelpalavraSECRETA.Text = string.Empty;
+            labelPONTOS.Text = string.Empty;
+
+            for (int i = 0; i < 10; i++)
+            {
+                vetorpalavraSECRETA[i] = "_";
+            }
+        }
+
+        private void buttonINICIA_Click(object sender, EventArgs e)
+        {
+            labelNOMEJOGADOR.Text = textBoxNOMEJOGADOR.Text;
+            textBoxNOMEJOGADOR.Text = string.Empty;
+            palavraSECRETA = textBoxPALAVRASECRETA.Text.ToUpper();
+            textBoxPALAVRASECRETA.Text = string.Empty;
+            labelPONTOS.Text = "0";
+            pictureBoxFORCA.Image = Properties.Resources.vazia;
+            panelCONFIG.Visible = false;
+            labelpalavraSECRETA.Text = string.Empty;
+            for (int i = 0; i < palavraSECRETA.Length; i++)
+            {
+                labelpalavraSECRETA.Text = labelpalavraSECRETA.Text + vetorpalavraSECRETA[i] + " ";
+            }
+        }
+
+        private void labelCONFIG_Click(object sender, EventArgs e)
+        {
+            panelCONFIG.Visible = true;
+        }
+
+        private void buttonTESTAR_Click(object sender, EventArgs e)
+        {
+            if (textBoxLETRA.Text != string.Empty)
+            {
+                letraTESTAR = textBoxLETRA.Text.ToUpper();
+                for (int i = 0; i < palavraSECRETA.Length; i++)
+                {
+                    if (letraTESTAR == palavraSECRETA.Substring(i, 1))
+                    {
+                        vetorpalavraSECRETA[i] = letraTESTAR;
+                        erro = 0;
+                    }
+                    else
+                    {
+                        // ...
+                    }
+                }
+                if (erro == 1)
+                {
+                    MessageBox.Show("Letra Incorreta!");
+                    pontos = pontos - 5;
+                    tentativas++;
+                    if (tentativas == 1)
+                    {
+                        pictureBoxFORCA.Image = Properties.Resources.cabeca;
+                    }
+                    if (tentativas == 2)
+                    {
+                        pictureBoxFORCA.Image = Properties.Resources.corpo;
+                    }
+                    if (tentativas == 3)
+                    {
+                        pictureBoxFORCA.Image = Properties.Resources.braco1;
+                    }
+                    if (tentativas == 4)
+                    {
+                        pictureBoxFORCA.Image = Properties.Resources.braco2;
+                    }
+                    if (tentativas == 5)
+                    {
+                        pictureBoxFORCA.Image = Properties.Resources.perna1;
+                    }
+                    if (tentativas == 6)
+                    {
+                        pictureBoxFORCA.Image = Properties.Resources.perna2;
+                    }
+                }
+                else
+                {
+                    pontos = pontos + 20;
+                }
+                erro = 1;
+                textBoxLETRA.Text = string.Empty;
+                labelpalavraSECRETA.Text = string.Empty;
+                for (int i = 0; i < palavraSECRETA.Length; i++)
+                {
+                    labelpalavraSECRETA.Text = labelpalavraSECRETA.Text + vetorpalavraSECRETA[i] + " ";
+                    if (vetorpalavraSECRETA[i] == palavraSECRETA.Substring(i, 1))
+                    {
+                        acerto++;
+                    }
+                }
+                if (tentativas == 6)
+                {
+                    MessageBox.Show("ACABOU O JOGO!");
+                }
+                if (acerto == palavraSECRETA.Length)
+                {
+                    MessageBox.Show("VOCÊ VENCEU!");
+                }
+                acerto = 0;
+                labelPONTOS.Text = pontos.ToString();
+                textBoxLETRA.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Informe UMA LETRA!");
+            }
+        }
+
+        private void labelX_Click(object sender, EventArgs e)
+        {
+            panelCONFIG.Visible = false;
+        }
+    }
+}
